3e435d1f48e40f70e69f7dd88c0951a90db6a7dd
Costantino <kostasprint@gmail.com>
2018-08-30 15:08:41 +0200

    publishing

---
+++ script/game.js
@@ -0,0 +1,14 @@
+$("#a1").hover(
+function() {
+    $("#bg").toggleClass('img1');
+});
+
+$("#a2").hover(
+    function() {
+        $("#bg").toggleClass('img2');
+    });
+
+$("#a3").hover(
+    function() {
+        $("#bg").toggleClass('img3');
+    });
