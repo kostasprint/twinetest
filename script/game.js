e7506dd925542cf82f5694cd76f876a209ed3e65
Costantino <kostasprint@gmail.com>
2018-08-30 19:03:33 +0200

    game

--- script/game.js
+++ script/game.js
@@ -1,24 +1,8 @@
-$("#a1").hover(
-function() {
-    $("#bg").toggleClass('img1');
-});
-
-$("#a2").hover(
-    function() {
-        $("#bg").toggleClass('img2');
-    });
-
-$("#a3").hover(
-    function() {
-        $("#bg").toggleClass('img3');
-    });
-
-$("#a4").hover(
-    function() {
-        $("#bg").toggleClass('img4');
-    });
-
-$("#a5").hover(
-    function() {
-        $("#bg").toggleClass('img5');
-    });
+function init_img_subs (alist, target) {
+ for (el of alist) {
+  $(el[0]).hover (
+   function () {
+    $(target).toggleClass(el[1]);
+   });
+ }
+}
