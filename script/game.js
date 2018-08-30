aa7200f6803d07f125d59762438b5a1c37ca739a
Costantino <kostasprint@gmail.com>
2018-08-30 19:27:12 +0200

    game

--- script/game.js
+++ script/game.js
@@ -1,6 +1,7 @@
 function init_img_subs (alist, target) {
  for (el of alist) {
    console.log (el);
+   console.log ("ciaone");
   $(el[0]).hover (
    function () {
     $(target).toggleClass(el[1]);
